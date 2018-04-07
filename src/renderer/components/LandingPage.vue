bcd2b409cee66d156dfd4d9c5afb6b6bf197d1a0
Dongasai <1514582970@qq.com>
2018-04-07 22:35:58 +0800

    first commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>欢迎使用</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page',
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
